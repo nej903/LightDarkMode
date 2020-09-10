18159f934a90dff45ccf23bc919ba4bc016605c2
= <=>
2020-09-10 14:54:23 -0500

    added h1 & changeTitle func

--- toggle.js
+++ toggle.js
@@ -3,11 +3,19 @@ const allATags = Array.from(document.querySelectorAll("a"));
 const body = document.querySelector("body");
 const header = document.querySelector("header");
 const section = document.querySelector("section");
-const myArr = [body, header, section, allATags];
+const title = document.querySelector("h1");
+const myArr = [body, header, section, allATags, title];
 
 toggle.addEventListener("click", () => {
   // flatten nested array
   myArr.flat(3).forEach((el) => {
     el.classList.toggle("dark");
   });
+  changeTitle(title);
 });
+
+const changeTitle = (title) => {
+  [...title.classList].includes("dark")
+    ? (title.innerText = "Dark Theme")
+    : (title.innerText = "Light Theme");
+};
